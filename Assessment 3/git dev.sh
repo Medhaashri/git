5c1c9ae846fa1cc1b123cbd3c5698337f766a799
Medhaashri <medhaashri.veeramanikanda@agilisium.com>
2022-04-08 14:34:59 +0530

    Second Commit

---
+++ Assessment 3/git dev.sh
@@ -0,0 +1,31 @@
+ git clone https://github.com/Medhaashri/git.git
+ #changing the folder
+ cd git
+#creating python file
+ nano git.py
+#adding into the file
+ git add .
+ git commit -m "Medhaa Shri"
+#pushing the file in to git
+ git push
+ #adding changes to that python file
+  nano git.py                
+ git add . 
+ git commit -m "SHRI"
+#pushing it to python
+ git push
+#creating a branch "develop"
+ git checkout -b develop
+ git branch
+#adding to that branch
+git add .
+git commit -m "git"
+#pulling in to git
+ git pull
+ git checkout -b main   
+ git checkout main   
+ #pulling in to develop branch
+ git pull develop    
+ git checkout develop   
+ git push            
+ 
--- git.py
+++
@@ -1,3 +0,0 @@
-
-print('u r awsome')
-print('super')
